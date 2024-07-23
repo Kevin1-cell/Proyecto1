ea838eef1d765bbe2a211b1f4c0ce90eca537fbd
Kevin1-cell <aguirre.kevin.6135@eam.edu.co>
2024-07-22 19:11:55 -0500

    inicial commit

---
+++ src/proyecto1/Proyecto1.java
@@ -0,0 +1,71 @@
+
+package proyecto1;
+
+/**
+ * Programa que permite instanciar un objeto y cambiar las propiedades de una matriz
+ * @author Kevin Santiago Aguirre Giraldo
+ */
+public class Proyecto1 {
+    public static void main (String [] args){
+        
+        Proyecto1 proyecto1 = new Proyecto1();
+        
+        String [][] molde = proyecto1.consultarMolde();
+        System.out.println("Molde principal");      
+        for(int i = 0; i < molde.length; i++){
+            for (int j = 0; j < molde[i].length; j++){
+                System.out.print(molde[i][j]+"");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("Molde lleno: ");
+        proyecto1.llenarMolde(molde);
+        proyecto1.vertirPlastico(molde);
+        for(int i = 0; i < molde.length; i++){
+            for(int j = 0; j < molde[i].length; j++){
+                System.out.print(molde[i][j]+"");
+            }
+            System.out.println();
+        }
+        
+        
+        
+    }
+    
+    public String [][] consultarMolde(){
+        
+        String [][] molde = {
+            {"-", "#", "-"},
+            {"#", "#", "#"},
+            {"-", "#", "-"},
+            {"-", "#", "-"},
+            {"-", "#", "-"},
+        };
+        return molde;
+    }
+    
+    public void llenarMolde(String [][] molde){
+        
+        for(int i = 0; i < molde.length; i++){
+            for(int j = 0; j < molde[i].length; j++){
+                if(molde[i][j].equals("-")){
+                    molde[i][j] = " -------- ";
+                }
+            }
+        }
+    }
+    
+    public void vertirPlastico(String [][] molde){
+        
+        for(int i = 0; i < molde.length; i++){
+            for (int j = 0; j < molde[i].length; j++){
+                if(molde[i][j].equals("#")){
+                    molde[i][j] = " plastico ";
+                }
+            }
+        }
+    }
+
+    
+}
